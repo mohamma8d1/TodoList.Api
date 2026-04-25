f97cfc913d5680bdb29aa192d7a24e134fa5decb
mohamma8d1 <mmmirnoori@gmail.com>
2026-04-26 02:28:22 +0330

    Malomg Dto and Response of Token  for Auth System

---
+++ TodoList.Application/DTOs/LoginUserDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoList.Application.DTOs;
+
+public record LoginUserDto
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
