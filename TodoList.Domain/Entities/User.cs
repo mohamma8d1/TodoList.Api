e95dc2627f3063444b7860c130f56941df8195a5
mohamma8d1 <mmmirnoori@gmail.com>
2026-04-25 16:34:26 +0330

    add User Entity

---
+++ TodoList.Domain/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace TodoList.Domain.Entities;
+
+public class User
+{
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public string Email { get; set; } = string.Empty;
+    public string UserName { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;    
+
+    public ICollection<TodoItem> Todos { get; set; } = new List<TodoItem>();
+}
