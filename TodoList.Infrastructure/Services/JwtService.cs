33b4073726a1a1227d1762f95a3395df91957ef4
mohamma8d1 <mmmirnoori@gmail.com>
2026-04-26 02:15:03 +0330

    Jwt services complete

---
+++ TodoList.Infrastructure/Services/JwtService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System;       
+using System.Security.Claims;
+using System.Text;
+using TodoList.Domain.Entities;
+
+namespace TodoList.Infrastructure.Services;
+
+public interface IJwtService
+{
+    string GenerateToken(User user);
+}
+
+public class JwtService(IConfiguration configuration) : IJwtService
+{
+    public string GenerateToken(User user)
+    {
+
+        var claims = new[]
+        {
+            new Claim(ClaimTypes.NameIdentifier, user.Id),
+            new Claim(ClaimTypes.Name, user.UserName ?? ""),
+            new Claim(ClaimTypes.Email, user.Email ?? "")
+        };
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Secret"]!));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(
+                issuer: configuration["Jwt:Issuer"],
+                audience: configuration["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddDays(1),
+                signingCredentials: creds
+            );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
